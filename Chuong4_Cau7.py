ee38778d917dfeea1b20ab627d89734ce8f2bd5f
tat224 <tuan_dpm245512@student.agu.edu.vn>
2025-12-04 22:35:07 +0700

    baitapchuong4

---
+++ Chuong4_Cau7.py
@@ -0,0 +1,5 @@
+def ():
+    xa = float(input("Nhap xA: ")); ya = float(input("Nhap yA: "))
+    xb = float(input("Nhap xB: ")); yb = float(input("Nhap yB: "))
+    d = math.sqrt((xa - xb) ** 2 + (ya - yb) ** 2)
+    print("Do dai AB =", d)
